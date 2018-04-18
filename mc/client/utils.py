f3269a086f74993f7f775771e5d0a4d17ffd95bf
Bekele Habtetsega Moges <31346839+habimog@users.noreply.github.com>
2018-04-18 08:35:28 +0200

    Create utils.py

---
+++ mc/client/utils.py
@@ -0,0 +1,47 @@
+import subprocess
+from uuid import getnode as get_mac
+
+SERVERS = {
+	"trident1.vlab.cs.hioa.no" : "128.39.120.89",
+	"trident2.vlab.cs.hioa.no" : "128.39.120.90",
+	"trident3.vlab.cs.hioa.no" : "128.39.120.91"
+}
+
+CLIENT_MESSAGE = {
+	"request" : {
+		"temperature" : False,
+		"migration" : False
+	},
+	"vm" : {
+                "mac" : "",
+                "target" : ""
+        }
+}
+
+SERVER_MESSAGE = {
+	"trident1.vlab.cs.hioa.no" : 0.0,
+	"trident2.vlab.cs.hioa.no" : 0.0,
+	"trident3.vlab.cs.hioa.no" : 0.0
+}
+
+def getHostName():
+	hostName = ""
+	for key, value in SERVERS.items():	
+		host = subprocess.check_output("sudo traceroute %s | tail -n+2 | awk '{ print $2 }' | head -1" % (value), shell=True).decode('UTF-8').rstrip("\n")
+		print("host = {} key = {} value = {}".format(host, key, value))
+		if(host in SERVERS):
+			hostName = host
+			break
+
+	return hostName
+
+def getHostIp(hostName):
+	hostIp = ""
+	if hostName in SERVERS:
+		hostIp = SERVERS[hostName]
+	
+	return hostIp
+
+def getVmMac():
+	mac = get_mac()
+	return ':'.join(("%012x" % mac)[i:i+2] for i in range(0, 12, 2))
