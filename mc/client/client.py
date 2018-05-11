e43c93f2de25038eca0ee59274fddf05ff42dad4
Bekele Habtetsega Moges <habimog@gmail.com>
2018-05-11 18:16:45 +0200

    Update client.py

--- mc/client/client.py
+++ mc/client/client.py
@@ -25,7 +25,7 @@ class Client(object):
 		time.sleep(1200)
 		while True:
 			# Wake VM randomly
-			rand_time = random.randint(120, 180)
+			rand_time = random.randint(300, 600)
 			print("Rand Time = {}".format(rand_time))
 			time.sleep(rand_time)
 
