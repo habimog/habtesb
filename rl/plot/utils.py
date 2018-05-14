530a2c259bd4629b043e4cc480a67eb48cfab9dc
Bekele Habtetsega Moges <habimog@gmail.com>
2018-05-14 08:07:14 +0200

    Update utils.py

--- rl/plot/utils.py
+++ rl/plot/utils.py
@@ -40,7 +40,7 @@ def getPlotData():
 			# Receive response
 			print("waiting to receive")
 			sock.settimeout(5.0)
-			data, address = sock.recvfrom(512)
+			data, address = sock.recvfrom(4096*8)
 			sock.settimeout(None)
 			server_message[host] = json.loads(data.decode('utf-8'))
 			print("received: {} from {}".format(server_message[host], address))
